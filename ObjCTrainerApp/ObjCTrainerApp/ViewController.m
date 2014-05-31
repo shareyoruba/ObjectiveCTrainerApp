cc72be3a5cbe9e43e535eb097920cf28f60daefb
shareyoruba <shareyoruba@gmail.com>
2014-05-31 03:24:44 -0400

    initial project commit
    initial project creation for the obj c trainer app

---
+++ ObjCTrainerApp/ObjCTrainerApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjCTrainerApp
+//
+//  Created by Mikaila Akeredolu on 5/31/14.
+//  Copyright (c) 2014 Mikaila Akeredolu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
